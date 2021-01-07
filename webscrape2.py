6d4c7bc1e071b0979cb136869cc002316ff7a75b
David <mahannahd@gmail.com>
2021-01-06 20:06:20 -0600

    yea

--- webscrape2.py
+++ webscrape2.py
@@ -53,6 +53,21 @@ class Webscrape2:
         c = [value for value in lem_tok_1 if value in lem_tok_2]
         return len(c) / max(len(lem_tok_1), len(lem_tok_2)) * 100
 
+    def GET_UA():
+        uastrings = ["Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36",\
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/600.1.25 (KHTML, like Gecko) Version/8.0 Safari/600.1.25",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",\
+            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",\
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/600.1.17 (KHTML, like Gecko) Version/7.1 Safari/537.85.10",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko",\
+            "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:33.0) Gecko/20100101 Firefox/33.0",\
+            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.104 Safari/537.36"\
+        ]
+ 
+    return random.choice(uastrings)
+
     def searchQuizletDeck(self, link, text):
         headers = {"User-Agent": "Mozilla/5.0 (Linux; U; Android 4.2.2; he-il; NEO-X5-116A Build/JDQ39) AppleWebKit/534.30 ("
                          "KHTML, like Gecko) Version/4.0 Safari/534.30"}
@@ -81,7 +96,8 @@ class Webscrape2:
 
     def quizletScrape(self, text):
         print("joe")
-        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text))
+        headers={'User-Agent': GET_UA()}
+        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text), headers=headers)
         soup = BeautifulSoup(src.content, "html.parser")
         print(soup.prettify())
         google_links = []

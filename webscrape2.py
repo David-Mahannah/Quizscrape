0b697bea42af8bb3ed6a5015ec5b84a9e8a8939e
David <mahannahd@gmail.com>
2021-01-08 14:33:59 -0600

    syntax error

--- webscrape2.py
+++ webscrape2.py
@@ -115,7 +115,7 @@ class Webscrape2:
             }
         
         #headers={'User-Agent': self.GET_UA()}
-        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text), cert="myprojectenv/lib/python3.8/site-packages/certifi/cacert.pem" proxies=proxies, verify="crawlera-ca.crt")
+        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text), cert="myprojectenv/lib/python3.8/site-packages/certifi/cacert.pem", proxies=proxies, verify="crawlera-ca.crt")
         soup = BeautifulSoup(src.content, "html.parser")
 
         print(soup.prettify())

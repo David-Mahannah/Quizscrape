def777fab5c6a9c535402ba3ff4340fae83f3666
David <mahannahd@gmail.com>
2021-01-08 19:42:14 -0600

    p

--- webscrape2.py
+++ webscrape2.py
@@ -115,7 +115,7 @@ class Webscrape2:
             }
         
         #headers={'User-Agent': self.GET_UA()}
-        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text), proxies=proxies, verify="crawlera-ca.crt")
+        src = requests.get("https://google.com/search?q={0} site:Quizlet.com".format(text), cert="/etc/ssl/certs/ca-certificates.crt", proxies=proxies, verify="crawlera-ca.crt")
         soup = BeautifulSoup(src.content, "html.parser")
 
         print(soup.prettify())

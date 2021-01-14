9f054dc975b272f6354b238769a93929f234973c
David <mahannahd@gmail.com>
2021-01-13 23:18:47 -0600

    hiuhiuh

--- webscrape3.py
+++ webscrape3.py
@@ -11,6 +11,7 @@ import nltk
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import sentiwordnet as swn
 
+# Crawls google search results
 class CrawlerWorker(multiprocessing.Process):
     def __init__(self, spider, result_queue, text):
         multiprocessing.Process.__init__(self)
@@ -27,14 +28,11 @@ class CrawlerWorker(multiprocessing.Process):
 
     def run(self):
         self.crawler.crawl(self.spider, self.text)
-        print("LOG: starting :GOL")
         self.crawler.start()
-        print("LOG: stopping :GOL")
         self.crawler.stop()
-        print("LOG: putting in queue :GOL")
         self.result_queue.put(self.items)
-        print("LOG: done :GOL")
 
+# Crawls quizlet.com decks
 class QuizletWorker(multiprocessing.Process):
     def __init__(self, spider, result_queue, text, url):
         multiprocessing.Process.__init__(self)
@@ -52,62 +50,33 @@ class QuizletWorker(multiprocessing.Process):
 
     def run(self):
         self.crawler.crawl(self.spider, self.text, self.url)
-        print("LOG: starting :GOL")
         self.crawler.start()
-        print("LOG: stopping :GOL")
         self.crawler.stop()
-        print("LOG: putting in queue GOL")
         self.result_queue.put(self.items)
-        print("LOG: done :GOL")
 
 def scrapeDaGoog(text):
     result_queue = Queue()
     crawler = CrawlerWorker(GoogSpider, result_queue, text)
     crawler.start()
-    #crawler.stop()
-    #print("Alive? ", crawler.is_alive())
-    #print("---------------------------")
     out = []
-    #print(result_queue.get())
-    #print("CHECKPOINT 1")
-    #print(result_queue.get())
-    #return result_queue.get()[0]['link'][:8]
     for item in result_queue.get()[0]['link']:
-        print("Hellow")
-        #yield item
-    #    print(item.url)
         out.append(item.url)
 
-    #print("CHECKPOINT 2")
-    #crawler.stop()
-    #print("CHECKPOINT 3")
-    # crawler.terminate()
     return out
 
 
 def quizletScrape(text, url):
     result = Queue()
     crawler = QuizletWorker(QuizletSpider, result, text, url)
-    print("oy")
     crawler.start()
-    #print("yey")
-
     out = []
-    #print(len(result.get()))
     for card_holder in result.get():
-        print("----------")
         for i in card_holder['cards']:
             out.append(i)
-            print(i['front'], i['back'])
-        #print(card.back)
-        #print("----------")
-    
-    #print("we out here")
+
     if len(out) > 0:
-        print("Found something")
         return out
     else:
-        print("Nothing found")
         return None;
 
 
@@ -121,7 +90,7 @@ class Cards:
     def __str__(self):
         return self.question + ":" + self.result
 
-
+# Compares two strings by tokenizing them
 def compareS(string1, string2):
     wnl = WordNetLemmatizer()
     lem_tok_1 = [wnl.lemmatize(token) for token in nltk.word_tokenize(string1)]
@@ -130,51 +99,44 @@ def compareS(string1, string2):
     return len(c) / max(len(lem_tok_1), len(lem_tok_2)) * 100
 
 
-#if __name__ == '__main__':
-def run(text):
-    #print("=====================================================================================")
-    data = scrapeDaGoog(text)
-    print(data)
-    counter = 1
+# Main method to run.
+def run(text, minimum_similarity=60):
+    # Collect links from scrapeDaGoog method
+    google_links = scrapeDaGoog(text)
+    
+    # Run quizletScrape on each link in the deck
     out = []
-    for link in data:
-        if "https://quizlet.com" in link and link:
-            print("ITERATION ", counter)
-            out.extend(quizletScrape(text, link.split("url?q=", 1)[1]))
-            counter = counter + 1
+    for link in google_links:
+        # Since the links are unfiltered we need to select the ones that
+        # are quizlet decks. 
+        if "https://quizlet.com" in link:
+            try:
+                out.extend(quizletScrape(text, link.split("url?q=", 1)[1]))
     
-    print(out)
-
-    minimum_error = 60
-    
+    # Smash all the cards into a similarity algorithm and keep the
+    # cards that are above the minimum_error
     matches = []
     for card in out:
         front = card['front']
         back = card['back']
         url = card['link']
-
         c1 = compareS(front, text)
         c2 = compareS(back, text)
-        if (c1 > minimum_error):
+        if (c1 > minimum_similarity):
             matches.append(Cards(front, back, url, round(c1, 2)))
-        elif (c2 > minimum_error):
+        elif (c2 > minimum_similarity):
             matches.append(Cards(front, back, url, round(c2, 2)))
-   
-    #for card in matches:
-    #    print(card.question, card.result, card.similarity, card.resource)
 
+    # Bubble sort in ascending order of similarity
     n = len(matches)
     for i in range(n):
         for j in range(0, n-i-1):
             if matches[j].similarity < matches[j+1].similarity:
                 matches[j], matches[j+1] = matches[j+1], matches[j]
 
-
     return matches
-    #print(matches)
-    #print(out)
 
 
-
+# Debug and Testing
 if __name__ == '__main__':
     run("What is the capital of Germany?")
